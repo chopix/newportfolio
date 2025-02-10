71e2beb7492e91c435d4bd933445c8e5e5f6bc71
chopix <fresserslegend@gmail.com>
2025-02-10 22:59:55 +0100

    english

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@
 export default defineNuxtConfig({
  app: {
 					head: {
-									title: 'chopix - fullstack dev',
+									title: 'dmitriy - fullstack dev',
 									meta: [{ name: 'freelancehunt', content: '1896eb1c026e952' }],
 									link: [
 													{
